34020f7cc1c7c68a14f0bdbce1811c9d24e5a14c
karo1818 <karo.madrigal.morera@gmail.com>
2023-04-24 19:52:09 -0600

    ultima actualizacion cuadros

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,47 @@
 import '../styles/App.css';
-import freeCodeCampLogo from "../images/logo.png";
-import ListaDeTareas from './ListaDeTareas';
 import Navbar from './NavBar';
 import Welcome from './Welcome';
+import boostrap from "../images/Boostrap.webp"
+import css from "../images/Css.webp"
+import html from "../images/Html.webp"
+import javascript from "../images/Javascript.webp"
+import react from "../images/React.webp"
 
 function App() {
   return (
 
-
     <div className="App">
 
-        <Navbar/> /*De esta manera llamo un componente */
+        <Navbar/> /
         <Welcome/>
 
       <div className='aplicacion-tareas'>
-        <div className='freecodecamp-logo-contenedor'>
-          
-
-        </div>
-          <div className='tareas-lista-principal'>
-            <h1>Mis tareas</h1>
-              
-              <ListaDeTareas/>
-              
+        
+          <div className='principal-container'>
+           <div className='tittle'><h1>This project is made with</h1></div>
+            <div className='tareas-lista-principal'>
+            <img className='tools' src={css} />
+            <img className='tools' src={html} />
+            <img className='tools' src={javascript} />
+            <img className='tools' src={react} />
+            <img className='tools' src={boostrap} />
+            </div>
 
           </div>
+
+          <div className='principal-container'>
+           <div className='tittle'><h1>This project is made with</h1></div>
+            <div className='tareas-lista-principal'>
+            <img className='tools' src={css} />
+            <img className='tools' src={html} />
+            <img className='tools' src={javascript} />
+            <img className='tools' src={react} />
+            <img className='tools' src={boostrap} />
+            </div>
+
+          </div>
+          
+
 
       </div>
      
